2ced01a8736132cb1dbf2ac365e0dff1536c1e80
robert <robert@gymglish.com>
2017-09-27 17:52:56 +0300

    Created OnClick listener for the banner button

--- app/src/main/java/com/soomla/robertlesser/soomla/MainActivity.java
+++ app/src/main/java/com/soomla/robertlesser/soomla/MainActivity.java
@@ -2,12 +2,26 @@ package com.soomla.robertlesser.soomla;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bannerButton = findViewById(R.id.banner_button);
+        bannerButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        final int viewId = view.getId();
+        switch (viewId){
+            case R.id.banner_button:
+                break;
+        }
     }
 }
