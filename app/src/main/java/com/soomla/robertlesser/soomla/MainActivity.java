6fa01bcc13f0bca78d7e00b770886c797d2860ae
robert <robert@gymglish.com>
2017-09-27 19:51:59 +0300

    Added a banner on click of the banner button

--- app/src/main/java/com/soomla/robertlesser/soomla/MainActivity.java
+++ app/src/main/java/com/soomla/robertlesser/soomla/MainActivity.java
@@ -4,8 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.facebook.ads.*;
 
 public class MainActivity extends Activity implements View.OnClickListener {
+
+    private static String PLACEMENT_ID = "CAROUSEL_IMG_SQUARE_APP_INSTALL#YOUR_PLACEMENT_ID";
+
+    private AdView adView;
+    private LinearLayout adContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +22,16 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         Button bannerButton = findViewById(R.id.banner_button);
         bannerButton.setOnClickListener(this);
+
+        adContainer = findViewById(R.id.banner_container);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (adView != null) {
+            adView.destroy();
+        }
+        super.onDestroy();
     }
 
     @Override
@@ -21,6 +39,16 @@ public class MainActivity extends Activity implements View.OnClickListener {
         final int viewId = view.getId();
         switch (viewId){
             case R.id.banner_button:
+                if (adView == null) {
+                    // Instantiate an AdView view
+                    adView = new AdView(this, PLACEMENT_ID, AdSize.BANNER_HEIGHT_50);
+
+                    // Add the ad view to your activity layout
+                    adContainer.addView(adView);
+
+                    // Request an ad
+                    adView.loadAd();
+                }
                 break;
         }
     }
